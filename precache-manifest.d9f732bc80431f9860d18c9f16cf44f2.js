f63699c629209bd83b158e068af2f00eb73a78a0
paramesh <chattaparameswara@gmail.com>
2020-06-06 06:43:21 +0530

    Updates

---
+++ precache-manifest.d9f732bc80431f9860d18c9f16cf44f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9783a396916d0c1ac84223ba8e381374",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "05563c1cd9518875be68",
+    "url": "/react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7618d7ef5218bb5bd44f",
+    "url": "/react/static/css/main.99e2ca21.chunk.css"
+  },
+  {
+    "revision": "05563c1cd9518875be68",
+    "url": "/react/static/js/2.0320bc91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.0320bc91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7618d7ef5218bb5bd44f",
+    "url": "/react/static/js/main.07a3143f.chunk.js"
+  },
+  {
+    "revision": "9d958efef5b9b43c3fca",
+    "url": "/react/static/js/runtime-main.5c17064e.js"
+  }
+]);
